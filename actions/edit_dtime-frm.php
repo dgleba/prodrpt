c538a64dc6cb2851d718ae6073d3567b0b3577c9
dgleba <dgleba@gmail.com>
2015-04-16 09:45:55 -0400

    26 - 2015-04-16 - 1. add multiple autocomplete to 'who is on it' field

--- actions/edit_dtime-frm.php
+++ actions/edit_dtime-frm.php
@@ -37,14 +37,54 @@
             });
         </script>
 
+
         <script type="text/javascript">
+            // multiple autocomplete for "who is on it" field.
+            // multiple-remote autocomplete http://jqueryui.com/autocomplete/#multiple-remote   2013-11-07_Thu_15.55-PM
+            // php from: http://www.jensbits.com/2010/03/29/jquery-ui-autocomplete-widget-with-php-and-mysql/
             $(function() {
-                $("#whos_on_it_cid").autocomplete({
-                    source: "actions/pr_who_list_ac.php",
-                    minLength: 2,
-                    //select: function(event, ui) {
-                    //     $('#machinenum_cid').val(ui.item.id) 
-                    // }
+                function split(val) {
+                    return val.split(/,\s*/);
+                }
+                function extractLast(term) {
+                    return split(term).pop();
+                }
+                $("#whos_on_it_cid")
+                        // don't navigate away from the field on tab when selecting an item
+                        .bind("keydown", function(event) {
+                    if (event.keyCode === $.ui.keyCode.TAB &&
+                            $(this).data("ui-autocomplete").menu.active) {
+                        event.preventDefault();
+                    }
+                })
+                        .autocomplete({
+                    source: function(request, response) {
+                        $.getJSON("actions/pr_who_list_ac.php", {
+                            term: extractLast(request.term)
+                        }, response);
+                    },
+                    search: function() {
+                        // custom minLength
+                        var term = extractLast(this.value);
+                        if (term.length < 2) {
+                            return false;
+                        }
+                    },
+                    focus: function() {
+                        // prevent value inserted on focus
+                        return false;
+                    },
+                    select: function(event, ui) {
+                        var terms = split(this.value);
+                        // remove the current input
+                        terms.pop();
+                        // add the selected item
+                        terms.push(ui.item.value);
+                        // add placeholder to get the comma-and-space at the end
+                        terms.push("");
+                        this.value = terms.join(", ");
+                        return false;
+                    }
                 });
             });
         </script>
