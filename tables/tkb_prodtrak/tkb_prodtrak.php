6caf23b220c29c1289fc93c829c6a7addc2fca75
dgleba <dgleba@gmail.com>
2015-06-26 15:11:44 -0400

    32 - 2015-06-25 - added trackberry test

---
+++ tables/tkb_prodtrak/tkb_prodtrak.php
@@ -0,0 +1,23 @@
+<?php
+
+class tables_tkb_prodtrak {
+
+    function __sql__() {
+        return
+            "SELECT *,  from_unixtime(Time)  as Utimeformated
+            FROM `tkb_prodtrak` 
+            order by id desc
+            ";
+    }
+	
+	      //Restrict Non-admin users to read only on the Users table
+      function getPermissions(){
+          $auth =& Dataface_AuthenticationTool::getInstance();
+          $user =& $auth->getLoggedInUser();
+          if ( $user and  $user->val('Role') != 'ADMIN' ){
+          return Dataface_PermissionsTool::READ_ONLY();
+      }
+    }
+
+ }
+ 
