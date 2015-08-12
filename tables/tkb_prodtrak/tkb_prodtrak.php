e3c37bbe2b29f350df9b97948e534d91648d9eb0
dgleba <dgleba@gmail.com>
2015-08-12 10:15:33 -0400

    35 - 2015-08-11 - trakberry data page changes

--- tables/tkb_prodtrak/tkb_prodtrak.php
+++ tables/tkb_prodtrak/tkb_prodtrak.php
@@ -4,14 +4,42 @@ class tables_tkb_prodtrak {
 
     function __sql__() {
         return
-            "SELECT *,  from_unixtime(time)  as Part_made_at, (cycletime + downtime) as last_time_delta, 
+            "SELECT *,  from_unixtime(part_timestamp) , (cycletime + downtime) as last_time_delta, 
 			  IF((downtime + cycletime) < 120, (downtime + cycletime) ,  NULL) as ctime, 
 			  IF((downtime + cycletime) > 600, downtime,  NULL) as dtime 
 			  FROM `tkb_prodtrak` 
 			  order by id desc
             ";
     }
-    	
+    
+/*
+            "SELECT *,  from_unixtime(part_timestamp) , (cycletime + downtime) as last_time_delta, 
+			  IF((downtime + cycletime) < 120, (downtime + cycletime) ,  NULL) as ctime, 
+			  IF((downtime + cycletime) > 600, downtime,  NULL) as dtime 
+			  FROM `tkb_prodtrak` 
+			  order by id desc
+            ";
+
+            "SELECT *,  from_unixtime(part_timestamp) ,  
+			  if ( (last_time_diff between 40 and 120)  , (last_time_diff) ,  NULL) as ctime, 
+			  IF((last_time_diff) > 600, last_time_diff,  NULL) as dtime 
+			  FROM `tkb_prodtrak` 
+			  order by id desc
+            ";
+
+        SELECT *,  from_unixtime(part_timestamp) ,  
+			  if (  
+			        (if (last_time_diff > 40) , (last_time_diff) ,  NULL)
+			     (last_time_diff < 120) , (last_time_diff) ,  NULL)
+			     as ctime, 
+			  
+			  IF((last_time_diff) > 600, last_time_diff,  NULL) as dtime 
+			  FROM `tkb_prodtrak` 
+			  order by id desc
+			  
+*/
+
+	
 	      //Restrict Non-admin users to read only on the Users table
       function getPermissions(){
           $auth =& Dataface_AuthenticationTool::getInstance();
